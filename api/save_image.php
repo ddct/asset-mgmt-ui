56cc5ae8037c40d65d3bde1df39a1349a6288781
Basit Pathan <basitpathan@users.noreply.github.com>
2019-05-17 16:04:00 +0100

    update

--- api/save_image.php
+++ api/save_image.php
@@ -1,5 +1,5 @@
 <?php
-$folder = "/webapp_that_takes_pictures/uploads/images/";
+$folder = "/asset-mgmt-ui/uploads/images/";
 $destinationFolder = $_SERVER['DOCUMENT_ROOT'] . $folder; // you may need to adjust to your server configuration
 $maxFileSize = 2 * 1024 * 1024;
 
